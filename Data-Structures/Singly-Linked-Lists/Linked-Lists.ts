793ae6a13e0aff8dc3318b9b4fcef439af83c88b
EdwinMHZ <53926422+EdwinMHZ@users.noreply.github.com>
2023-11-21 08:23:10 -0600

    Intro Linked Lists

---
+++ Data-Structures/Singly-Linked-Lists/Linked-Lists.ts
@@ -0,0 +1,18 @@
+/*
+    What's a linked list?
+    A data structure that contains a head, tail and length property
+    Linked Lists consits of nodes, and each node has a value and
+    a pointer to another node or null
+
+    Comparisons with Arrays
+    Lists
+    - Do not have indexes
+    - Connected via node with a next pointer
+    - Random access is not allowed
+
+    Arrays
+    - Indexed in order.
+    - Insertion and deletion can be expensive
+    - Can quickly be accessed at a specific index
+
+*/

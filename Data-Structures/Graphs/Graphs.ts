7619dc846c882fa37b9906900d1c263df1e1f2d6
EdwinMHZ <53926422+EdwinMHZ@users.noreply.github.com>
2023-12-28 12:31:56 -0600

    Graphs essential methods

--- Data-Structures/Graphs/Graphs.ts
+++ Data-Structures/Graphs/Graphs.ts
@@ -27,3 +27,30 @@
       Query             O(|V| + |E|)        O(1)
       Storage           O(|V| + |E|)        O(|V^2|)
 */
+
+class Graph {
+    adjacencyList: Map<string, Array<string>>;
+    constructor() {
+        this.adjacencyList = new Map();
+    }
+    addVertex(vertex: string) {
+        this.adjacencyList.set(vertex, []);
+    }
+    addEdge(vertex1: string, vertex2: string) {
+        this.adjacencyList.get(vertex1)!.push(vertex2);
+        this.adjacencyList.get(vertex2)!.push(vertex1);
+    }
+    removeEdge(vertex1: string, vertex2: string) {
+        let res: string[] = this.adjacencyList.get(vertex1)!.filter(v => v != vertex2);
+        this.adjacencyList.set(vertex1, res);
+        res = this.adjacencyList.get(vertex2)!.filter(v => v != vertex1);
+        this.adjacencyList.set(vertex2, res);
+    }
+    removeVertex(vertex: string) {
+        while(this.adjacencyList.get(vertex)!.length) {
+            const adjacentVertex = this.adjacencyList.get(vertex)!.pop();
+            this.removeEdge(adjacentVertex!, vertex);
+        }
+        this.adjacencyList.delete(vertex);
+    }
+}

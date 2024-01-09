a4c89553f935ec99d3cb230c5e7db08af5992aef
EdwinMHZ <53926422+EdwinMHZ@users.noreply.github.com>
2024-01-09 08:56:07 -0600

    Dynamic Programming Intro

---
+++ Dynamic-Programming/DynamicProgramming.ts
@@ -0,0 +1,15 @@
+/*
+    A method for solving complex problem by
+    breaking it down into a collection of simpler
+    subproblems, solving each of those subproblems
+    just once and storing their solution.
+
+    IT ONLY WORKS ON PROBLEMS WITH:
+    -OPTIMAL SUBSTRUCTURE: A problem is to have optimal
+    substructure if an optimal solution can be constructed
+    from optimal solutions of its subproblems.
+    -OVERLAPPING SUBPROBLEMS: A problem is said to have
+    overlapping subproblems if it can be broken down
+    into subproblems which are reused several times.
+    Example: Fibonnaci Sequence
+*/
